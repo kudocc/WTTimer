1b0c30baa9c1bd729eaaa61c314a5472d70f3c4e
yuanrui <yuanrui@yuanrui.local>
2015-03-18 17:28:44 +0800

    Initial Commit

---
+++ weakTargetTimerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weakTargetTimerDemo
+//
+//  Created by yuanrui on 15-3-18.
+//  Copyright (c) 2015年 KudoCC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
